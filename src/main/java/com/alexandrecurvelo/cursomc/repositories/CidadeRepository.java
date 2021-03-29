96f706e0054bab6d9a4bf47bf56f766f72f6c4e8
Alexandre Oliveira Curvelo <allexolic@gmail.com>
2021-03-29 18:54:31 -0300

    Adicionado dominio - Estado e Cidade

---
+++ src/main/java/com/alexandrecurvelo/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.alexandrecurvelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandrecurvelo.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
---
+++ src/main/java/com/alexandrecurvelo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.alexandrecurvelo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.alexandrecurvelo.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
